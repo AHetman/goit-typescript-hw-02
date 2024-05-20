0ee34394c40da2e82d352b4a37c8d21d6de85093
AHetman <101942183+AHetman@users.noreply.github.com>
2024-05-20 15:22:31 +0300

    config

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import Modal from "react-modal";
 
 Modal.setAppElement("#root");
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
