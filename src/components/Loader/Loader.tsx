7a83887f952a54b98e72b7c3d0e3286a2b2c0209
AHetman <101942183+AHetman@users.noreply.github.com>
2024-05-20 17:26:27 +0300

    upd2

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,12 +2,15 @@
 import { InfinitySpin } from "react-loader-spinner";
 import css from "./Loader.module.css";
 
-const Loader = ({ loading }: { loading: boolean }) => {
+interface LoaderProps {
+  loading: boolean;
+}
+
+const Loader: React.FC<LoaderProps> = ({ loading }) => {
   return (
     <InfinitySpin
-      className={css.infinitySpin}
       visible={loading}
-      width="200"
+      width={String(200)}
       color="#4fa94d"
       ariaLabel="infinity-spin-loading"
     />
